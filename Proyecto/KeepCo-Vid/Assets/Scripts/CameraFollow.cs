d54fd9dec5c4628163a6e10019942238fcc01994
javicm3 <javier.muriel@live.u-tad.com>
2020-04-22 14:55:59 +0200

    ProyectoSubido

---
+++ Proyecto/KeepCo-Vid/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    Vector3 cameraPosition;
+    [SerializeField] int posicionCamara;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.position + new Vector3(0, posicionCamara, 0);
+        
+    }
+}
